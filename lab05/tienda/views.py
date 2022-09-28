7e79c0408040c991f0a37c07a84443f917850970
AramosA0 <aldoramosaro@gmail.com>
2022-09-28 17:58:32 -0500

    lab06

--- lab05/tienda/views.py
+++ lab05/tienda/views.py
@@ -1,3 +1,25 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Producto,Categoria,Cliente
+
+def index(request):
+    product_list = Producto.objects.all()
+    lista_categoria = Categoria.objects.all()
+    context = {
+        'product_list':product_list,
+        'lista_categoria':lista_categoria
+    }
+    return render(request,"index.html", context)
+
+def producto(request, productoid):
+    producto = Producto.objects.get(id=productoid)
+    context={
+        'producto':producto
+    }
+    return render(request,"producto.html", context)
+
+def categoria(request,categoriaid):
+    categoria = Categoria.objects.get(id=categoriaid)
