aaee7f6167fdaeed6cfd50fed1d2aad0605591c2
AramosA0 <aldoramosaro@gmail.com>
2022-09-30 14:10:37 -0500

    lab6 completo

--- lab05/tienda/urls.py
+++ lab05/tienda/urls.py
@@ -7,4 +7,5 @@ app_name = 'tienda'
 urlpatterns = [
     path('',views.index, name='index'),
     path('producto/<int:productoid>',views.producto, name='producto'),
+    path('categoria/<int:categoriaid>',views.categoria, name='categoria'),
 ]
--- lab05/tienda/views.py
+++ lab05/tienda/views.py
@@ -16,10 +16,20 @@ def index(request):
 
 def producto(request, productoid):
     producto = Producto.objects.get(id=productoid)
+    lista_categoria = Categoria.objects.all()
     context={
-        'producto':producto
+        'producto':producto,
+        'lista_categoria':lista_categoria
     }
     return render(request,"producto.html", context)
 
 def categoria(request,categoriaid):
+    lista_categoria = Categoria.objects.all()
     categoria = Categoria.objects.get(id=categoriaid)
+    lista_productos = Producto.objects.filter(categoria = categoriaid)
+    context = {
+        'categoria':categoria,
+        'lista_categoria':lista_categoria,
+        'lista_productos':lista_productos
+    }
+    return render(request, "categoria.html", context)
